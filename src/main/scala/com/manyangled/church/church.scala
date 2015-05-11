a5eb2b683b481a682f6199f06d98a21bbbbd9299
Erik Erlandson <eerlands@redhat.com>
2015-05-11 13:45:23 -0700

    bit more aggressive scoping

--- src/main/scala/com/manyangled/church/church.scala
+++ src/main/scala/com/manyangled/church/church.scala
@@ -3,7 +3,7 @@ package com.manyangled
 object church {
   import scala.language.higherKinds
 
-  import church_detail._
+  import church$detail._
 
   sealed trait Integer {
     type Inc <: Integer
@@ -12,36 +12,40 @@ object church {
     type Sub[K <: Integer] <: Integer
     type Neg <: Integer
 
-    def value: Int
+    private[church] def value: Int
   }
 
   def construct[N <: Integer :Constructable] = implicitly[Constructable[N]].construct()
 
   def value[N <: Integer :Constructable] = construct[N].value
 
-  type _0 = Zero
-  type _1 = _0#Inc
-  type _2 = _1#Inc
-  type _3 = _2#Inc
-  type _4 = _3#Inc
-  type _5 = _4#Inc
-  type _6 = _5#Inc
-  type _7 = _6#Inc
-  type _8 = _7#Inc
-  type _9 = _8#Inc
+  object constants {
+    type _0 = Zero
 
-  type _neg1 = _0#Dec
-  type _neg2 = _neg1#Dec
-  type _neg3 = _neg2#Dec
-  type _neg4 = _neg3#Dec
-  type _neg5 = _neg4#Dec
-  type _neg6 = _neg5#Dec
-  type _neg7 = _neg6#Dec
-  type _neg8 = _neg7#Dec
-  type _neg9 = _neg8#Dec
+    type _1 = _0#Inc
+    type _2 = _1#Inc
+    type _3 = _2#Inc
+    type _4 = _3#Inc
+    type _5 = _4#Inc
+    type _6 = _5#Inc
+    type _7 = _6#Inc
+    type _8 = _7#Inc
+    type _9 = _8#Inc
 
+    type _neg1 = _0#Dec
+    type _neg2 = _neg1#Dec
+    type _neg3 = _neg2#Dec
+    type _neg4 = _neg3#Dec
+    type _neg5 = _neg4#Dec
+    type _neg6 = _neg5#Dec
+    type _neg7 = _neg6#Dec
+    type _neg8 = _neg7#Dec
+    type _neg9 = _neg8#Dec
+  }
 
-  object church_detail {
+  object church$detail {
+    import church.constants._
+
     trait Constructable[+T] {
       def construct(): T
     }
@@ -59,7 +63,7 @@ object church {
       type Sub[K <: Integer] = N#Sub[K]#Inc
       type Neg = N#Neg#Dec
 
-      def value = {
+      private[church] def value = {
         implicitly[Constructable[N]].construct().value + 1
       }
     }
@@ -71,7 +75,7 @@ object church {
       type Sub[K <: Integer] = N#Sub[K]#Dec
       type Neg = N#Neg#Inc
 
-      def value = {
+      private[church] def value = {
         implicitly[Constructable[N]].construct().value - 1
       }
     }
@@ -83,7 +87,7 @@ object church {
       type Sub[K <: Integer] = K#Neg
       type Neg = Zero
 
-      def value = 0
+      private[church] def value = 0
     }
 
     implicit val constructable0: Constructable[_0] = Constructable { new _0 }
@@ -106,6 +110,5 @@ object church {
     implicit val constructable7n: Constructable[_neg7] = Constructable { new _neg7 }
     implicit val constructable8n: Constructable[_neg8] = Constructable { new _neg8 }
     implicit val constructable9n: Constructable[_neg9] = Constructable { new _neg9 }
-
   }
 }
