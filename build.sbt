d3abd9e1d3e8e8c26aaf7d2ed2e112b9b6232596
Erik Erlandson <eerlands@redhat.com>
2016-01-13 15:28:36 -0700

    wip - factor out code generation to codegen subproject

--- build.sbt
+++ build.sbt
@@ -1,27 +1,36 @@
 name := "unit4s"
 
-organization := "com.manyangled"
-
-version := "0.0.1"
-
-resolvers ++= Seq(
-  Resolver.sonatypeRepo("releases"),
-  Resolver.sonatypeRepo("snapshots")
+lazy val commonSettings = Seq(
+  organization := "com.manyangled",
+  version := "0.0.1",
+  scalaVersion := "2.11.6",
+  resolvers ++= Seq(
+    Resolver.sonatypeRepo("releases"),
+    Resolver.sonatypeRepo("snapshots")
+  ),
+  libraryDependencies ++= Seq(
+    "org.scalatest" %% "scalatest" % "2.2.4" % Test,
+    "com.github.scala-incubator.io" %% "scala-io-core" % "0.4.3",
+    "com.github.scala-incubator.io" %% "scala-io-file" % "0.4.3"
+  ),
+  licenses += ("Apache-2.0", url("http://opensource.org/licenses/Apache-2.0")),
+  scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature"),
+  scalacOptions in (Compile, doc) ++= Seq("-doc-root-content", baseDirectory.value+"/root-doc.txt"),
+  fork := true
 )
 
-scalaVersion := "2.11.6"
+seq(commonSettings: _*)
 
 libraryDependencies ++= Seq(
-  "com.chuusai" %% "shapeless" % "2.1.0",
-  "org.scalatest" %% "scalatest" % "2.2.4" % Test,
-  "com.github.scala-incubator.io" %% "scala-io-core" % "0.4.3",
-  "com.github.scala-incubator.io" %% "scala-io-file" % "0.4.3"
+  "com.chuusai" %% "shapeless" % "2.1.0"
 )
 
-licenses += ("Apache-2.0", url("http://opensource.org/licenses/Apache-2.0"))
+lazy val unit4s = project in file(".")
 
-scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature")
-
-scalacOptions in (Compile, doc) ++= Seq("-doc-root-content", baseDirectory.value+"/root-doc.txt")
-
-fork := true
+lazy val codegen = project
+  .settings(commonSettings :_*)
+  .settings(
+    initialCommands in console := """
+      |import com.manyangled.codegen._
+    """.stripMargin
+  )
---
+++ codegen/src/main/scala/com/manyangled/codegen/codegen.scala
@@ -0,0 +1,105 @@
+package com.manyangled
+
+object codegen {
+  def writeString(str: String, path: String) {
+    import scalax.io._
+    Resource.fromFile(path).write(str)(Codec.UTF8)
+  }
+
+  val abase: Int = 'a'.toInt
+  def letter(j: Int) = (abase + j).toChar
+
+  def rhsFile(V: Int, fName: String) {
+    val defList = rhsImplicitDefSeq(V)
+    println(s"generated ${defList.length} implicit function definitions")
+    val defs = defList.mkString("\n")
+    val ccDef = rhsClassDef(V)
+    val code = s"""|/* THIS FILE WAS MACHINE GENERATED, DO NOT EDIT */
+      |package com.manyangled.unit4s
+      |
+      |import scala.language.implicitConversions
+      |
+      |import com.manyangled.church.{ Integer, IntegerValue }
+      |import Integer.{ _0, _1 }
+      |import Unit.factor
+      |
+      |$ccDef
+      |
+      |object RHS {
+      |$defs
+      |}""".stripMargin
+    scalax.file.Path.fromString(fName).deleteIfExists()
+    writeString(code, fName)
+  }
+
+  def rhsClassDef(V: Int): String = {
+    val uS = (1 to V).map { j => s"U$j" }
+    val uaS = (1 to V).map { j => s"UA$j" }
+    val unS = (1 to V).map { j => s"UN$j" }
+    val pS = (1 to V).map { j => s"P$j" }
+    val uSig = (uS ++ uaS ++ unS).map { u => s"$u <: Unit[$u]" }
+    val pSig = pS.map { p => s"$p <: Integer" }
+    val ccSig = (uSig ++ pSig).mkString(", ")
+    val udef = (1 to V).map { j => s"udef${j}: UnitDef[U$j]" }.mkString(", ")
+    val udefa = (1 to V).map { j => s"udef${j}a: UnitDef[UA$j]" }.mkString(", ")
+    val udefn = (1 to V).map { j => s"udef${j}n: UnitDef[UN$j]" }.mkString(", ")
+    val ivdef = (1 to V).map { j => s"iv${j}: IntegerValue[P$j]" }.mkString(", ")
+    s"""case class RHS[$ccSig](value: Double)(implicit
+      |  $udef,
+      |  $udefa,
+      |  $udefn,
+      |  $ivdef)""".stripMargin
+  }
+
+  // V is maximum unit valence
+  def rhsImplicitDefSeq(V: Int): Seq[String] = {
+    val rU = (1 to V).map { j => s"U$j" }
+    val rudef = (1 to V).map { j => s"udef${j}: UnitDef[U${j}]" }.mkString(", ")
+    val defs = scala.collection.mutable.ArrayBuffer.empty[String]
+    (1 to V).foreach { v =>
+      // v is current valence of input: >= 1, <= V
+      val iU = (0 until v).map { j => "U" + letter(j) }
+      val iP = (0 until v).map { j => "P" + letter(j) }
+      val iUP = iU.zip(iP)
+      val iSig = iUP.map { case (u, p) => s"$u, $p" }.mkString(", ")
+      val fSig = ((rU ++ iU).map { tn => s"$tn <: Unit[$tn]" } ++ iP.map { tn => s"$tn <: Integer" }).mkString(", ")
+      val pP = iP ++ Seq.fill(V - v) { "_0" }
+      val iudef = (0 until v).map { j => s"udef${letter(j)}: UnitDef[U${letter(j)}]" }.mkString(", ")
+      val iiv = (0 until v).map { j => s"iv${letter(j)}: IntegerValue[P${letter(j)}]" }.mkString(", ")
+      (0 to v).foreach { a =>
+        // a is current number aligned, >= 0, <= v
+        // u is current number un-aligned: >= 0, <= v
+        val u = v - a
+        val uU = (a until v).map { j => s"U${letter(j)}" } ++ Seq.fill(V - u) { "U$" }
+        // (V) choose (a) ways to be aligned
+        (0 until V).combinations(a).foreach { aj =>
+          val aU = Seq.tabulate(V) { j => if (aj.contains(j)) s"U${j + 1}" else "U$" }
+          val rhsSig = (rU ++ aU ++ uU ++ pP).mkString(", ")
+          (0 until v).combinations(a).foreach { aij =>
+            val uij = (0 until v).filter { j => !aij.contains(j) }
+            val rune = (for (ju <- uij; j <- (0 until V)) yield { s"ne${letter(ju)}${j + 1}: U${letter(ju)}#RU =!= U${j + 1}#RU" }).mkString(", ")
+            aij.permutations.foreach { ajp =>
+              val rueq = ajp.zip(aj).map { case (ja, ju) => s"eq${letter(ja)}${ju + 1}: U${letter(ja)}#RU =:= U${ju + 1}#RU" }.mkString(", ")
+              val eqne = if (rune.isEmpty) rueq else if (rueq.isEmpty) rune else (rune + ",\n    " + rueq)
+              val fSeq = ajp.zip(aj).map { case (ja, ju) => s"(factor[U${letter(ja)}, U${ju + 1}], iv${letter(ja)}.value)" }
+              val vCode = if (fSeq.length == 0) "val v = uv.value" else s"""val fp = Seq(${fSeq.mkString(", ")})
+                |    val v = fp.foldLeft(uv.value) { case (v, (f, p)) => v * math.pow(f, p) }""".stripMargin
+              val fCode = s"""
+                |  implicit def rhs$$v${v}a${a}c${aj.mkString("")}p${ajp.mkString("")}[$fSig](uv: UnitValue$v[$iSig])(implicit
+                |    $rudef,
+                |    $iudef,
+                |    $iiv,
+                |    $eqne): RHS[$rhsSig] = {
+                |    $vCode
+                |    RHS[$rhsSig](v)
+                |  }""".stripMargin
+              //println(fCode)
+              defs += fCode
+            }
+          }
+        }
+      }
+    }
+    defs
+  }
+}
