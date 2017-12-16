6c01a6481330c2304d255150cdfed3bd459bc246
Erik Erlandson <eerlands@redhat.com>
2017-12-16 13:08:50 -0700

    remove typeString code

--- src/main/scala/coulomb/Quantity.scala
+++ src/main/scala/coulomb/Quantity.scala
@@ -30,31 +30,6 @@ import infra._
 
 import test._
 
-/*
-// return a human-readable type string for type argument 'T'
-// typeString[Int] returns "Int"
-def typeString[T :TypeTag]: String = {
-  def work(t: Type): String = {
-    t match { case TypeRef(pre, sym, args) =>
-      val ss = sym.toString.stripPrefix("trait ").stripPrefix("class ").stripPrefix("type ")
-      val as = args.map(work)
-      if (ss.startsWith("Function")) {
-        val arity = args.length - 1
-        "(" + (as.take(arity).mkString(",")) + ")" + "=>" + as.drop(arity).head
-      } else {
-        if (args.length <= 0) ss else (ss + "[" + as.mkString(",") + "]")
-      }
-    }
-  }
-  work(typeOf[T])
-}
-
-// get the type string of an argument:
-// typeString(2) returns "Int"
-def typeString[T :TypeTag](x: T): String = typeString[T]
-
-*/
-
 trait IsMember[E, L] {
   type Out
 }
