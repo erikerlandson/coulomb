2e34a6f241acf54c275b39f79416dc1f0190d0d5
Erik Erlandson <eerlands@redhat.com>
2018-01-07 14:16:06 -0700

    companion methods

--- coulomb-temp-units/src/main/scala/coulomb/temp/Temperature.scala
+++ coulomb-temp-units/src/main/scala/coulomb/temp/Temperature.scala
@@ -20,6 +20,8 @@ import scala.language.implicitConversions
 
 import coulomb._
 import coulomb.unitops._
+import coulomb.infra._
+import coulomb.define._
 
 class Temperature[N, U] private[coulomb] (val value: N) extends AnyVal with Serializable {
   override def toString = s"Temperature($value)"
@@ -67,4 +69,21 @@ class Temperature[N, U] private[coulomb] (val value: N) extends AnyVal with Seri
 
   def to[N2, U2](implicit ubo: TempConverterOps[N, U, N2, U2]): Temperature[N2, U2] =
     new Temperature[N2, U2](ubo.cv12(value))
+}
+
+object Temperature {
+  def apply[N, U](v: N)(implicit t2k: DerivedTemp[U]) = new Temperature[N, U](v)
+
+  def showUnit[U](implicit t2k: DerivedTemp[U], ustr: UnitString[U]): String = ustr.abbv
+
+  def showUnitFull[U](implicit t2k: DerivedTemp[U], ustr: UnitString[U]): String = ustr.full
+
+  def fromQuantity[N, U](q: Quantity[N, U])(implicit t2k: DerivedTemp[U]): Temperature[N, U] =
+    new Temperature[N, U](q.value)
+
+  def toQuantity[N, U](t: Temperature[N, U]): Quantity[N, U] = new Quantity[N, U](t.value)
+
+  implicit def implicitlyConvertTemperature[N1, U1, N2, U2](t1: Temperature[N1, U1])(implicit
+      cv12: TempConverter[N1, U1, N2, U2]): Temperature[N2, U2] =
+    new Temperature[N2, U2](cv12(t1.value))
 }
