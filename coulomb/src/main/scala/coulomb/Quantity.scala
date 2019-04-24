85b6a205b14a1deed86d65f134b29ff590945985
Erik Erlandson <eerlands@redhat.com>
2019-04-24 11:29:12 -0700

    fix typos in scaladoc for quantity

--- coulomb/src/main/scala/coulomb/Quantity.scala
+++ coulomb/src/main/scala/coulomb/Quantity.scala
@@ -54,7 +54,7 @@ class Quantity[N, U](val value: N) extends AnyVal with Serializable {
    * @tparam U2 the unit type of the rhs quantity. Must be convertable to U, or a compile-time type
    * error will result.
    * @param rhs the right hand quantity in the sum.
-   * @return this + rhs, expressed in units (N, U).
+   * @return this+rhs, expressed in units (N,U).
    */
   def +[N2, U2](rhs: Quantity[N2, U2])(implicit uc: UnitConverter[N, U, N2, U2]): Quantity[N, U] =
     new Quantity[N, U](uc.n1.plus(value, uc.cv21(rhs.value)))
@@ -65,7 +65,7 @@ class Quantity[N, U](val value: N) extends AnyVal with Serializable {
    * @tparam U2 the unit type of the rhs quantity. Must be convertable to U, or a compile-time type
    * error will result.
    * @param rhs the right hand quantity in the difference.
-   * @return this - rhs, expressed in units (N, U).
+   * @return this-rhs, expressed in units (N,U).
    */
   def -[N2, U2](rhs: Quantity[N2, U2])(implicit uc: UnitConverter[N, U, N2, U2]): Quantity[N, U] =
     new Quantity[N, U](uc.n1.minus(value, uc.cv21(rhs.value)))
@@ -75,7 +75,7 @@ class Quantity[N, U](val value: N) extends AnyVal with Serializable {
    * @tparam N2 the numeric type of the rhs quantity
    * @tparam U2 the unit type of the rhs quantity.
    * @param rhs the right hand quantity in the product.
-   * @return this * rhs, with numeric type N and unit type U*U2.
+   * @return this*rhs, with numeric type N and unit type U*U2.
    */
   def *[N2, U2](rhs: Quantity[N2, U2])(implicit um: UnitMultiply[N, U, N2, U2]): Quantity[N, um.RT12] =
     new Quantity[N, um.RT12](um.n1.times(value, um.cn21(rhs.value)))
@@ -85,7 +85,7 @@ class Quantity[N, U](val value: N) extends AnyVal with Serializable {
    * @tparam N2 the numeric type of the rhs quantity
    * @tparam U2 the unit type of the rhs quantity.
    * @param rhs the right hand quantity.
-   * @return this / rhs, with numeric type N and unit type U/U2.
+   * @return this/rhs, with numeric type N and unit type U/U2.
    */
   def /[N2, U2](rhs: Quantity[N2, U2])(implicit ud: UnitDivide[N, U, N2, U2]): Quantity[N, ud.RT12] =
     new Quantity[N, ud.RT12](ud.n1.div(value, ud.cn21(rhs.value)))
@@ -103,7 +103,7 @@ class Quantity[N, U](val value: N) extends AnyVal with Serializable {
    * @tparam N2 the numeric type of the rhs quantity
    * @tparam U2 the unit type of the rhs quantity. Must be convertable to U, or a compile-time type
    * error will result.
-   * @param rhs the right hand quantity in the sum.
+   * @param rhs the right hand quantity.
    * @return true if rhs is equal to this quantity (after conversion to types N,U), false otherwise
    */
   def ===[N2, U2](rhs: Quantity[N2, U2])(implicit uc: UnitConverter[N, U, N2, U2]): Boolean =
@@ -114,7 +114,7 @@ class Quantity[N, U](val value: N) extends AnyVal with Serializable {
    * @tparam N2 the numeric type of the rhs quantity
    * @tparam U2 the unit type of the rhs quantity. Must be convertable to U, or a compile-time type
    * error will result.
-   * @param rhs the right hand quantity in the sum.
+   * @param rhs the right hand quantity.
    * @return true if rhs is not equal to this quantity (after conversion to types N,U), false otherwise
    */
   def =!=[N2, U2](rhs: Quantity[N2, U2])(implicit uc: UnitConverter[N, U, N2, U2]): Boolean =
@@ -125,7 +125,7 @@ class Quantity[N, U](val value: N) extends AnyVal with Serializable {
    * @tparam N2 the numeric type of the rhs quantity
    * @tparam U2 the unit type of the rhs quantity. Must be convertable to U, or a compile-time type
    * error will result.
-   * @param rhs the right hand quantity in the sum.
+   * @param rhs the right hand quantity.
    * @return true if this quantity is less than the right quantity (after conversion to types N,U), false otherwise
    */
   def <[N2, U2](rhs: Quantity[N2, U2])(implicit uc: UnitConverter[N, U, N2, U2]): Boolean =
@@ -136,7 +136,7 @@ class Quantity[N, U](val value: N) extends AnyVal with Serializable {
    * @tparam N2 the numeric type of the rhs quantity
    * @tparam U2 the unit type of the rhs quantity. Must be convertable to U, or a compile-time type
    * error will result.
-   * @param rhs the right hand quantity in the sum.
+   * @param rhs the right hand quantity.
    * @return true if this quantity is less than or equal to the right quantity (after conversion to types N,U), false otherwise
    */
   def <=[N2, U2](rhs: Quantity[N2, U2])(implicit uc: UnitConverter[N, U, N2, U2]): Boolean =
@@ -147,7 +147,7 @@ class Quantity[N, U](val value: N) extends AnyVal with Serializable {
    * @tparam N2 the numeric type of the rhs quantity
    * @tparam U2 the unit type of the rhs quantity. Must be convertable to U, or a compile-time type
    * error will result.
-   * @param rhs the right hand quantity in the sum.
+   * @param rhs the right hand quantity.
    * @return true if this quantity is greater than the right quantity (after conversion to types N,U), false otherwise
    */
   def >[N2, U2](rhs: Quantity[N2, U2])(implicit uc: UnitConverter[N, U, N2, U2]): Boolean =
@@ -158,7 +158,7 @@ class Quantity[N, U](val value: N) extends AnyVal with Serializable {
    * @tparam N2 the numeric type of the rhs quantity
    * @tparam U2 the unit type of the rhs quantity. Must be convertable to U, or a compile-time type
    * error will result.
-   * @param rhs the right hand quantity in the sum.
+   * @param rhs the right hand quantity.
    * @return true if this quantity is greater than or equal to the right quantity (after conversion to types N,U), false otherwise
    */
   def >=[N2, U2](rhs: Quantity[N2, U2])(implicit uc: UnitConverter[N, U, N2, U2]): Boolean =
