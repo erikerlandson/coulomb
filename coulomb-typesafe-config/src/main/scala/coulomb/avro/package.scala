f8c171f31ad00b2c7cea5fbed988a3a9b78bd3a5
Erik Erlandson <eerlands@redhat.com>
2019-05-18 08:14:00 -0700

    caching conversion parse results for efficiency

--- coulomb-typesafe-config/src/main/scala/coulomb/avro/package.scala
+++ coulomb-typesafe-config/src/main/scala/coulomb/avro/package.scala
@@ -5,19 +5,42 @@ import scala.language.implicitConversions
 import scala.reflect.runtime.universe.TypeTag
 
 import org.apache.avro.generic.GenericData
+import org.apache.avro.Schema
 
 import coulomb.parser.unitops.UnitTypeString
+import coulomb.unitops.UnitString
 import coulomb.parser.QuantityParser
 
 package object avro {
   implicit class EnhanceGenericRecord(rec: GenericData.Record) {
     val schema = rec.getSchema
-    def getQuantity[N :TypeTag, U :UnitTypeString](qp: QuantityParser)(field: String): Quantity[N, U] = {
-      val unit = schema.getField(field).getObjectProp("unit")
+
+    def getQuantity[N, U](qp: QuantityParser)(field: String)(implicit
+        n: spire.math.Numeric[N],
+        ut: UnitTypeString[U],
+        us: UnitString[U]): Quantity[N, U] = {
+      val fld = schema.getField(field)
+      val ufld = fld.getObjectProp("unit").asInstanceOf[String]
+      val uq = us.full
+      val cq = fld.getObjectProp(uq)
+      val coef = if (cq != null) cq.asInstanceOf[Double] else {
+        val c = qp.coefficient[U](ufld).get.toDouble
+        fld.addProp(uq, c)
+        c
+      }
       val raw = rec.get(field)
-      qp[N, U](s"${raw} ${unit}").get
+      if (raw == null) throw new Exception(s"null record field: $field")
+      val dbv = fld.schema.getType match {
+        case tpe if (tpe == Schema.Type.FLOAT) => raw.asInstanceOf[Float].toDouble
+        case tpe if (tpe == Schema.Type.DOUBLE) => raw.asInstanceOf[Double]
+        case tpe if (tpe == Schema.Type.INT) => raw.asInstanceOf[Int].toDouble
+        case tpe if (tpe == Schema.Type.LONG) => raw.asInstanceOf[Long].toDouble
+        case _ => throw new Exception(s"non-numeric field type: ${fld.schema.getType}")
+      }
+      Quantity[N, U](n.fromType[Double](coef * dbv))
     }
-    def getQuantity[N :TypeTag, U :UnitTypeString](field: String)(implicit qp: QuantityParser): Quantity[N, U] =
-      getQuantity(qp)(field)
+
+    def getQuantity[N :spire.math.Numeric, U :UnitString :UnitTypeString](field: String)(implicit qp: QuantityParser): Quantity[N, U] =
+      getQuantity[N, U](qp)(field)
   }
 }
