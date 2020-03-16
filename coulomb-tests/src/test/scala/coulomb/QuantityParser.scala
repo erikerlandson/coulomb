0f970c8b4efb469059775a96ca933562c352d8f1
Erik Erlandson <eerlands@redhat.com>
2020-03-16 13:57:23 -0700

    port quantity parser tests to utest

--- coulomb-tests/src/test/scala/coulomb/QuantityParser.scala
+++ coulomb-tests/src/test/scala/coulomb/QuantityParser.scala
@@ -23,17 +23,6 @@ import coulomb.temp._
 
 import coulomb.validators.CoulombValidators._
 
-object QuantityParserTests extends TestSuite {
-  val tests = Tests {
-    test("test1") {
-      assert(true)
-    }
-  }
-}
-
-/*
-import org.scalatest.QMatchers._
-
 object ConfigIntegration {
   import com.typesafe.config.ConfigFactory
   import coulomb.parser.QuantityParser
@@ -50,36 +39,41 @@ object ConfigIntegration {
   val conf = confTS.withQuantityParser(qp)
 }
 
-class QuantityParserSpec extends FlatSpec with Matchers {
+object QuantityParserTests extends TestSuite {
   import coulomb.parser.QuantityParser
   import ConfigIntegration._
 
-  it should "return configured unit quantities" in {
-    conf.getQuantity[Double, Second]("duration").get.shouldBeQ[Double, Second](60)
-    conf.getQuantity[Int, Giga %* Byte]("memory").get.shouldBeQ[Int, Giga %* Byte](100)
-    conf.getQuantity[Float, Mega %* Byte %/ Second]("bandwidth").get.shouldBeQ[Float, Mega %* Byte %/ Second](10)
-  }
+  val tests = Tests {
+    test("return configured unit quantities") {
+      assert(
+        conf.getQuantity[Double, Second]("duration").get.isValidQ[Double, Second](60),
+        conf.getQuantity[Int, Giga %* Byte]("memory").get.isValidQ[Int, Giga %* Byte](100),
+        conf.getQuantity[Float, Mega %* Byte %/ Second]("bandwidth").get.isValidQ[Float, Mega %* Byte %/ Second](10)
+      )
+    }
 
-  it should "return convertable unit quantities" in {
-    conf.getQuantity[Double, Minute]("duration").get.shouldBeQ[Double, Minute](1)
-    conf.getQuantity[Int, Giga %* Bit]("memory").get.shouldBeQ[Int, Giga %* Bit](800)
-    conf.getQuantity[Float, Giga %* Bit %/ Minute]("bandwidth").get.shouldBeQ[Float, Giga %* Bit %/ Minute](4.8)
-  }
+    test("return convertable unit quantities") {
+      assert(
+        conf.getQuantity[Double, Minute]("duration").get.isValidQ[Double, Minute](1),
+        conf.getQuantity[Int, Giga %* Bit]("memory").get.isValidQ[Int, Giga %* Bit](800),
+        conf.getQuantity[Float, Giga %* Bit %/ Minute]("bandwidth").get.isValidQ[Float, Giga %* Bit %/ Minute](4.8)
+      )
+    }
 
-  it should "fail on incompatible units" in {
-    val try1 = conf.getQuantity[Double, Meter]("duration")
-    try1.isInstanceOf[scala.util.Failure[_]] should be (true)
-  }
+    test("fail on incompatible units") {
+      val try1 = conf.getQuantity[Double, Meter]("duration")
+      assert(try1.isInstanceOf[scala.util.Failure[_]])
+    }
 
-  it should "support ser/de" in {
-    import coulomb.scalatest.serde._
-    val expr = "3.14 gigabyte / second"
-    val qp1 = QuantityParser[Byte :: Second :: Giga :: Mega :: HNil]
-    val t1 = qp1[Float, Giga %* Byte %/ Second](expr).get
-    val qp2 = roundTripSerDe(qp1)
-    val t2 = qp2[Float, Giga %* Byte %/ Second](expr).get
-    (t1 === t2) shouldBe (true)
-    t2.shouldBeQ[Float, Giga %* Byte %/ Second](3.14)
+    test("support ser/de") {
+      import coulomb.scalatest.serde._
+      val expr = "3.14 gigabyte / second"
+      val qp1 = QuantityParser[Byte :: Second :: Giga :: Mega :: HNil]
+      val t1 = qp1[Float, Giga %* Byte %/ Second](expr).get
+      val qp2 = roundTripSerDe(qp1)
+      val t2 = qp2[Float, Giga %* Byte %/ Second](expr).get
+      assert(t1 === t2)
+      assert(t2.isValidQ[Float, Giga %* Byte %/ Second](3.14))
+    }
   }
 }
-*/
