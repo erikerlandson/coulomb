8a45ecc01928eb48467555b68b7bb686d9a4402f
Erik Erlandson <eerlands@redhat.com>
2022-02-27 13:22:21 -0700

    sub.scala

--- core/src/main/scala/coulomb/ops/standard/sub.scala
+++ core/src/main/scala/coulomb/ops/standard/sub.scala
@@ -18,79 +18,79 @@ package coulomb.ops.standard
 
 import scala.util.NotGiven
 
-import coulomb.ops.Add
+import coulomb.ops.Sub
 import coulomb.conversion.{ValueConversion, UnitConversion, ValueResolution}
 import coulomb.policy.ImplicitConversionsEnabled
 
-transparent inline given ctx_add_Double_1U[U]: Add[Double, U, Double, U] =
-    new Add[Double, U, Double, U]:
+transparent inline given ctx_sub_Double_1U[U]: Sub[Double, U, Double, U] =
+    new Sub[Double, U, Double, U]:
         type VO = Double
         type UO = U
-        def apply(vl: Double, vr: Double): Double = vl + vr
+        def apply(vl: Double, vr: Double): Double = vl - vr
 
-transparent inline given ctx_add_Float_1U[U]: Add[Float, U, Float, U] =
-    new Add[Float, U, Float, U]:
+transparent inline given ctx_sub_Float_1U[U]: Sub[Float, U, Float, U] =
+    new Sub[Float, U, Float, U]:
         type VO = Float
         type UO = U
-        def apply(vl: Float, vr: Float): Float = vl + vr
+        def apply(vl: Float, vr: Float): Float = vl - vr
 
-transparent inline given ctx_add_Long_1U[U]: Add[Long, U, Long, U] =
-    new Add[Long, U, Long, U]:
+transparent inline given ctx_sub_Long_1U[U]: Sub[Long, U, Long, U] =
+    new Sub[Long, U, Long, U]:
         type VO = Long
         type UO = U
-        def apply(vl: Long, vr: Long): Long = vl + vr
+        def apply(vl: Long, vr: Long): Long = vl - vr
 
-transparent inline given ctx_add_Int_1U[U]: Add[Int, U, Int, U] =
-    new Add[Int, U, Int, U]:
+transparent inline given ctx_sub_Int_1U[U]: Sub[Int, U, Int, U] =
+    new Sub[Int, U, Int, U]:
         type VO = Int
         type UO = U
-        def apply(vl: Int, vr: Int): Int = vl + vr
+        def apply(vl: Int, vr: Int): Int = vl - vr
 
-transparent inline given ctx_add_Double_2U[UL, UR](using
+transparent inline given ctx_sub_Double_2U[UL, UR](using
     ImplicitConversionsEnabled,
     NotGiven[UL =:= UR]
-        ): Add[Double, UL, Double, UR] =
+        ): Sub[Double, UL, Double, UR] =
     val c = coulomb.conversion.infra.coefficientDouble[UR, UL]
-    new Add[Double, UL, Double, UR]:
+    new Sub[Double, UL, Double, UR]:
         type VO = Double
         type UO = UL
-        def apply(vl: Double, vr: Double): Double = vl + (c * vr)
+        def apply(vl: Double, vr: Double): Double = vl - (c * vr)
 
-transparent inline given ctx_add_Float_2U[UL, UR](using
+transparent inline given ctx_sub_Float_2U[UL, UR](using
     ImplicitConversionsEnabled,
     NotGiven[UL =:= UR]
-        ): Add[Float, UL, Float, UR] =
+        ): Sub[Float, UL, Float, UR] =
     val c = coulomb.conversion.infra.coefficientFloat[UR, UL]
-    new Add[Float, UL, Float, UR]:
+    new Sub[Float, UL, Float, UR]:
         type VO = Float
         type UO = UL
-        def apply(vl: Float, vr: Float): Float = vl + (c * vr)
+        def apply(vl: Float, vr: Float): Float = vl - (c * vr)
 
-transparent inline given ctx_add_1V2U[V, UL, UR](using
+transparent inline given ctx_sub_1V2U[V, UL, UR](using
     ice: ImplicitConversionsEnabled,
     neu: NotGiven[UL =:= UR],
     ucv: UnitConversion[V, UR, UL],
     alg: Algebra[V]
-        ): Add[V, UL, V, UR] =
-    new Add[V, UL, V, UR]:
+        ): Sub[V, UL, V, UR] =
+    new Sub[V, UL, V, UR]:
         type VO = V
         type UO = UL
-        def apply(vl: V, vr: V): V = alg.add(vl, ucv(vr))
+        def apply(vl: V, vr: V): V = alg.sub(vl, ucv(vr))
 
-transparent inline given ctx_add_2V1U[VL, VR, U](using
+transparent inline given ctx_sub_2V1U[VL, VR, U](using
     ice: ImplicitConversionsEnabled,
     nev: NotGiven[VL =:= VR],
     vres: ValueResolution[VL, VR],
     vlvo: ValueConversion[VL, vres.VO],
     vrvo: ValueConversion[VR, vres.VO],
     alg: Algebra[vres.VO]
-        ): Add[VL, U, VR, U] =
-    new Add[VL, U, VR, U]:
+        ): Sub[VL, U, VR, U] =
+    new Sub[VL, U, VR, U]:
         type VO = vres.VO
         type UO = U
-        def apply(vl: VL, vr: VR): VO = alg.add(vlvo(vl), vrvo(vr))
+        def apply(vl: VL, vr: VR): VO = alg.sub(vlvo(vl), vrvo(vr))
 
-transparent inline given ctx_add_2V2U[VL, UL, VR, UR](using
+transparent inline given ctx_sub_2V2U[VL, UL, VR, UR](using
     ice: ImplicitConversionsEnabled,
     nev: NotGiven[VL =:= VR],
     neu: NotGiven[UL =:= UR],
@@ -99,9 +99,8 @@ transparent inline given ctx_add_2V2U[VL, UL, VR, UR](using
     vrvo: ValueConversion[VR, vres.VO],
     ucvo: UnitConversion[vres.VO, UR, UL],
     alg: Algebra[vres.VO]
-        ): Add[VL, UL, VR, UR] =
-    new Add[VL, UL, VR, UR]:
+        ): Sub[VL, UL, VR, UR] =
+    new Sub[VL, UL, VR, UR]:
         type VO = vres.VO
         type UO = UL
-        def apply(vl: VL, vr: VR): VO = alg.add(vlvo(vl), ucvo(vrvo(vr)))
-
+        def apply(vl: VL, vr: VR): VO = alg.sub(vlvo(vl), ucvo(vrvo(vr)))
