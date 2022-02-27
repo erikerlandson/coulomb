ba9e95be85b7020caeffdbf986ae35691b4469a3
Erik Erlandson <eerlands@redhat.com>
2022-02-27 12:02:23 -0700

    test addition

--- core/src/test/scala/coulomb/quantity.scala
+++ core/src/test/scala/coulomb/quantity.scala
@@ -19,7 +19,6 @@ import coulomb.testing.CoulombSuite
 class QuantitySuite extends CoulombSuite:
     import coulomb.*
     import coulomb.testing.units.{*, given}
-    import coulomb.conversion.standard.given
 
     test("lift via Quantity") {
         Quantity[Meter](3.14).assertQ[Double, Meter](3.14)
@@ -46,6 +45,8 @@ class QuantitySuite extends CoulombSuite:
     }
 
     test("toValue standard") {
+        import coulomb.conversion.standard.given
+
         1.withUnit[Meter].toValue[Int].assertQ[Int, Meter](1)
         1.withUnit[Meter].toValue[Long].assertQ[Long, Meter](1)
         1.withUnit[Meter].toValue[Float].assertQ[Float, Meter](1)
@@ -69,6 +70,7 @@ class QuantitySuite extends CoulombSuite:
     }
 
     test("toValue standard integral") {
+        import coulomb.conversion.standard.given
         import coulomb.conversion.standard.integral.given
 
         1.5f.withUnit[Meter].toValue[Int].assertQ[Int, Meter](1)
@@ -79,6 +81,8 @@ class QuantitySuite extends CoulombSuite:
     }
 
     test("toUnit standard") {
+        import coulomb.conversion.standard.given
+
         1.5f.withUnit[Minute].toUnit[Second].assertQ[Float, Second](90)
         1d.withUnit[Meter].toUnit[Yard].assertQD[Double, Yard](1.0936132983377078)
         // conversions on integral types require importing integral conversion rules
@@ -87,6 +91,7 @@ class QuantitySuite extends CoulombSuite:
     }
 
     test("toUnit standard integral") {
+        import coulomb.conversion.standard.given
         import coulomb.conversion.standard.integral.given
 
         1.withUnit[Minute].toUnit[Second].assertQ[Int, Second](60)
@@ -94,14 +99,110 @@ class QuantitySuite extends CoulombSuite:
     }
 
     test("implicit conversions") {
+        import coulomb.conversion.standard.given
+
         // implicit conversions only happen if you import them
         // https://docs.scala-lang.org/scala3/reference/contextual/conversions.html
         def f(q: Quantity[Double, Meter]): Double = q.value
         assertCE("f(1d.withUnit[Yard])")
+
+        // toUnit and toValue will operate, because they are explicit conversion request
         f(1d.withUnit[Yard].toUnit[Meter]).assertVTD[Double](0.9144)
+        f(1.withUnit[Meter].toValue[Double]).assertVTD[Double](1.0)
+
         object t {
+            // enabling implicit conversions should allow them
             import scala.language.implicitConversions
             import coulomb.conversion.standard.implicitConversion.given
             f(1d.withUnit[Yard]).assertVTD[Double](0.9144)
         }
+    }
+
+    test("addition standard strict") {
+        import coulomb.ops.standard.given
+        // importing conversions makes them available explicitly but they will
+        // not be available implicitly unless that is enabled separately
+        import coulomb.conversion.standard.given
+
+        // adding w/ same value and unit requires no implicit conversion
+        (1d.withUnit[Second] + 1d.withUnit[Second]).assertQ[Double, Second](2)
+        (1f.withUnit[Meter] + 1f.withUnit[Meter]).assertQ[Float, Meter](2)
+        (1L.withUnit[Kilogram] + 1L.withUnit[Kilogram]).assertQ[Long, Kilogram](2)
+        (1.withUnit[Meter / Second] + 1.withUnit[Meter / Second]).assertQ[Int, Meter / Second](2)
+
+        // changing either unit or value involves implicit conversion, should error out
+        assertCE("1d.withUnit[Meter] + 1d.withUnit[Yard]")
+        assertCE("1d.withUnit[Meter] + 1f.withUnit[Meter]")
+
+        // explicit conversion will still work
+        (1d.withUnit[Meter] + 1d.withUnit[Yard].toUnit[Meter]).assertQD[Double, Meter](1.9144)
+        (1d.withUnit[Meter] + 1f.withUnit[Meter].toValue[Double]).assertQD[Double, Meter](2)
+    }
+
+    test("addition standard") {
+        import coulomb.ops.standard.given
+        import coulomb.conversion.standard.given
+        import coulomb.conversion.standard.implicitConversion.given
+
+        // same value type, different units
+        (1d.withUnit[Kilo * Second] + 1d.withUnit[Second]).assertQD[Double, Kilo * Second](1.001)
+        (1f.withUnit[Meter] + 1f.withUnit[Yard]).assertQD[Float, Meter](1.9144)
+
+        // same unit, differing value types
+        (1d.withUnit[Meter] + 1f.withUnit[Meter]).assertQ[Double, Meter](2)
+        (1d.withUnit[Meter] + 1L.withUnit[Meter]).assertQ[Double, Meter](2)
+        (1d.withUnit[Meter] + 1.withUnit[Meter]).assertQ[Double, Meter](2)
+
+        (1f.withUnit[Meter] + 1d.withUnit[Meter]).assertQ[Double, Meter](2)
+        (1L.withUnit[Meter] + 1d.withUnit[Meter]).assertQ[Double, Meter](2)
+        (1.withUnit[Meter] + 1d.withUnit[Meter]).assertQ[Double, Meter](2)
+
+        (1f.withUnit[Meter] + 1d.withUnit[Meter]).assertQ[Double, Meter](2)
+        (1f.withUnit[Meter] + 1L.withUnit[Meter]).assertQ[Float, Meter](2)
+        (1f.withUnit[Meter] + 1.withUnit[Meter]).assertQ[Float, Meter](2)
+
+        (1d.withUnit[Meter] + 1f.withUnit[Meter]).assertQ[Double, Meter](2)
+        (1L.withUnit[Meter] + 1f.withUnit[Meter]).assertQ[Float, Meter](2)
+        (1.withUnit[Meter] + 1f.withUnit[Meter]).assertQ[Float, Meter](2)
+
+        // differing value and unit
+        (1d.withUnit[Kilogram] + 1f.withUnit[Pound]).assertQD[Double, Kilogram](1.45359237)
+        (1d.withUnit[Kilogram] + 1L.withUnit[Pound]).assertQD[Double, Kilogram](1.45359237)
+        (1d.withUnit[Kilogram] + 1.withUnit[Pound]).assertQD[Double, Kilogram](1.45359237)
+
+        (1f.withUnit[Kilogram] + 1d.withUnit[Pound]).assertQD[Double, Kilogram](1.45359237)
+        (1L.withUnit[Kilogram] + 1d.withUnit[Pound]).assertQD[Double, Kilogram](1.45359237)
+        (1.withUnit[Kilogram] + 1d.withUnit[Pound]).assertQD[Double, Kilogram](1.45359237)
+
+        (1f.withUnit[Kilogram] + 1f.withUnit[Pound]).assertQD[Float, Kilogram](1.45359237)
+        (1f.withUnit[Kilogram] + 1L.withUnit[Pound]).assertQD[Float, Kilogram](1.45359237)
+        (1f.withUnit[Kilogram] + 1.withUnit[Pound]).assertQD[Float, Kilogram](1.45359237)
+
+        (1f.withUnit[Kilogram] + 1f.withUnit[Pound]).assertQD[Float, Kilogram](1.45359237)
+        (1L.withUnit[Kilogram] + 1f.withUnit[Pound]).assertQD[Float, Kilogram](1.45359237)
+        (1.withUnit[Kilogram] + 1f.withUnit[Pound]).assertQD[Float, Kilogram](1.45359237)
+
+        // non convertible units should fail
+        assertCE("1d.withUnit[Meter] + 1d.withUnit[Second]")
+
+        // unsafe integral conversions should fail
+        assertCE("1.withUnit[Meter] + 1.withUnit[Yard]")
+        assertCE("1L.withUnit[Meter] + 1L.withUnit[Yard]")
+    }
+
+    test("addition standard integral") {
+        import coulomb.ops.standard.given
+        import coulomb.conversion.standard.given
+        import coulomb.conversion.standard.integral.given
+        import coulomb.conversion.standard.implicitConversion.given
+
+        (1L.withUnit[Second] + 1L.withUnit[Minute]).assertQ[Long, Second](61)
+        (1L.withUnit[Second] + 1.withUnit[Minute]).assertQ[Long, Second](61)
+        (1.withUnit[Second] + 1L.withUnit[Minute]).assertQ[Long, Second](61)
+        (1.withUnit[Second] + 1.withUnit[Minute]).assertQ[Int, Second](61)
+
+        (1L.withUnit[Meter] + 1L.withUnit[Yard]).assertQ[Long, Meter](1)
+        (1L.withUnit[Meter] + 1.withUnit[Yard]).assertQ[Long, Meter](1)
+        (1.withUnit[Meter] + 1L.withUnit[Yard]).assertQ[Long, Meter](1)
+        (1.withUnit[Meter] + 1.withUnit[Yard]).assertQ[Int, Meter](1)
     }
