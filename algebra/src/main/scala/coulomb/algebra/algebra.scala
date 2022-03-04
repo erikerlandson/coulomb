b966eaecc7d009d6f599957c4781ccd77b51e9d3
Arman Bilge <armanbilge@gmail.com>
2022-03-04 18:37:09 +0000

    Add algebra instances for Rational

--- algebra/src/main/scala/coulomb/algebra/algebra.scala
+++ algebra/src/main/scala/coulomb/algebra/algebra.scala
@@ -1,9 +1,25 @@
 package coulomb.algebra
 
 import algebra.ring.*
+import cats.kernel.*
 import coulomb.ops.standard.*
+import coulomb.rational.*
 
 given [A](using A: AdditiveSemigroup[A]): CanAdd[A] = A.plus(_, _)
 given [A](using A: AdditiveGroup[A]): CanSub[A] = A.minus(_, _)
 given [A](using A: MultiplicativeSemigroup[A]): CanMul[A] = A.times(_, _)
 given [A](using A: MultiplicativeGroup[A]): CanDiv[A] = A.div(_, _)
+
+given Field[Rational] with
+  def zero = Rational.const0
+  def one = Rational.const1
+  def plus(x: Rational, y: Rational) = x + y
+  override def minus(x: Rational, y: Rational) = x - y
+  def times(x: Rational, y: Rational) = x * y
+  def div(x: Rational, y: Rational) = x / y
+  def negate(x: Rational) = -x
+  override def pow(x: Rational, n: Int) = x.pow(n)
+
+given Hash[Rational] with
+  def eqv(x: Rational, y: Rational) = x == y
+  def hash(x: Rational) = x.hashCode
