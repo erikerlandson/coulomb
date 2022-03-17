2bee2c784d9f521450da454b4ff42cdd2c45fe65
Erik Erlandson <eerlands@redhat.com>
2022-03-17 05:50:47 -0700

    ValueResolution doens't need to be transparent inline

--- core/src/main/scala/coulomb/ops/resolution/standard.scala
+++ core/src/main/scala/coulomb/ops/resolution/standard.scala
@@ -19,66 +19,50 @@ package coulomb.ops.resolution
 object standard:
     import coulomb.ops.ValueResolution
 
-    transparent inline given ctx_VR_Double_Double: ValueResolution[Double, Double] =
-        new ValueResolution[Double, Double]:
-            type VO = Double
+    given ctx_VR_Double_Double: ValueResolution[Double, Double] with
+        type VO = Double
 
-    transparent inline given ctx_VR_Double_Float: ValueResolution[Double, Float] =
-        new ValueResolution[Double, Float]:
-            type VO = Double
+    given ctx_VR_Double_Float: ValueResolution[Double, Float] with
+        type VO = Double
 
-    transparent inline given ctx_VR_Double_Long: ValueResolution[Double, Long] =
-        new ValueResolution[Double, Long]:
-            type VO = Double
+    given ctx_VR_Double_Long: ValueResolution[Double, Long] with
+        type VO = Double
 
-    transparent inline given ctx_VR_Double_Int: ValueResolution[Double, Int] =
-        new ValueResolution[Double, Int]:
-            type VO = Double
+    given ctx_VR_Double_Int: ValueResolution[Double, Int] with
+        type VO = Double
 
-    transparent inline given ctx_VR_Float_Double: ValueResolution[Float, Double] =
-        new ValueResolution[Float, Double]:
-            type VO = Double
+    given ctx_VR_Float_Double: ValueResolution[Float, Double] with
+        type VO = Double
 
-    transparent inline given ctx_VR_Float_Float: ValueResolution[Float, Float] =
-        new ValueResolution[Float, Float]:
-            type VO = Float
+    given ctx_VR_Float_Float: ValueResolution[Float, Float] with
+        type VO = Float
 
-    transparent inline given ctx_VR_Float_Long: ValueResolution[Float, Long] =
-        new ValueResolution[Float, Long]:
-            type VO = Float
+    given ctx_VR_Float_Long: ValueResolution[Float, Long] with
+        type VO = Float
 
-    transparent inline given ctx_VR_Float_Int: ValueResolution[Float, Int] =
-        new ValueResolution[Float, Int]:
-            type VO = Float
+    given ctx_VR_Float_Int: ValueResolution[Float, Int] with
+        type VO = Float
 
-    transparent inline given ctx_VR_Long_Double: ValueResolution[Long, Double] =
-        new ValueResolution[Long, Double]:
-            type VO = Double
+    given ctx_VR_Long_Double: ValueResolution[Long, Double] with
+        type VO = Double
 
-    transparent inline given ctx_VR_Long_Float: ValueResolution[Long, Float] =
-        new ValueResolution[Long, Float]:
-            type VO = Float
+    given ctx_VR_Long_Float: ValueResolution[Long, Float] with
+        type VO = Float
 
-    transparent inline given ctx_VR_Long_Long: ValueResolution[Long, Long] =
-        new ValueResolution[Long, Long]:
-            type VO = Long
+    given ctx_VR_Long_Long: ValueResolution[Long, Long] with
+        type VO = Long
 
-    transparent inline given ctx_VR_Long_Int: ValueResolution[Long, Int] =
-        new ValueResolution[Long, Int]:
-            type VO = Long
+    given ctx_VR_Long_Int: ValueResolution[Long, Int] with
+        type VO = Long
 
-    transparent inline given ctx_VR_Int_Double: ValueResolution[Int, Double] =
-        new ValueResolution[Int, Double]:
-            type VO = Double
+    given ctx_VR_Int_Double: ValueResolution[Int, Double] with
+        type VO = Double
 
-    transparent inline given ctx_VR_Int_Float: ValueResolution[Int, Float] =
-        new ValueResolution[Int, Float]:
-            type VO = Float
+    given ctx_VR_Int_Float: ValueResolution[Int, Float] with
+        type VO = Float
 
-    transparent inline given ctx_VR_Int_Long: ValueResolution[Int, Long] =
-        new ValueResolution[Int, Long]:
-            type VO = Long
+    given ctx_VR_Int_Long: ValueResolution[Int, Long] with
+        type VO = Long
 
-    transparent inline given ctx_VR_Int_Int: ValueResolution[Int, Int] =
-        new ValueResolution[Int, Int]:
-            type VO = Int
+    given ctx_VR_Int_Int: ValueResolution[Int, Int] with
+        type VO = Int
