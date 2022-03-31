a795b93c08cc4de4b8e43f8c56649e5cdd78cfbd
Erik Erlandson <eerlands@redhat.com>
2022-03-31 11:39:37 -0700

    PowImplFP experiment

--- core/src/main/scala/coulomb/ops/standard/pow.scala
+++ core/src/main/scala/coulomb/ops/standard/pow.scala
@@ -26,16 +26,25 @@ import coulomb.ops.{Pow, SimplifiedUnit}
 import coulomb.rational.typeexpr
 import coulomb.ops.algebra.FractionalPower
 
+class PowImplFP[V, U, E, UOp](alg: FractionalPower[V], e: Double) extends Pow[V, U, E]:
+    type VO = V
+    type UO = UOp
+    def apply(q: Quantity[V, U]): Quantity[VO, UO] =
+        alg.pow(q.value, e).withUnit[UO]
+
 transparent inline given ctx_pow_FractionalPower[V, U, E](using
     alg: FractionalPower[V],
     su: SimplifiedUnit[U ^ E]
         ): Pow[V, U, E] =
+    new PowImplFP[V, U, E, su.UO](alg, typeexpr.double[E])
+    /*
     val e = typeexpr.double[E]
     new Pow[V, U, E]:
         type VO = V
         type UO = su.UO
         def apply(q: Quantity[V, U]): Quantity[VO, UO] =
             alg.pow(q.value, e).withUnit[UO]
+    */
 
 transparent inline given ctx_pow_MultiplicativeGroup[V, U, E](using
     nfp: NotGiven[FractionalPower[V]],
