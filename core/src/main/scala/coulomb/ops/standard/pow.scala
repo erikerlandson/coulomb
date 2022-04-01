b6a9de20195e368b5df30a8ec3d0e28691a2dbd4
Erik Erlandson <eerlands@redhat.com>
2022-04-01 13:34:34 -0700

    pow

--- core/src/main/scala/coulomb/ops/standard/pow.scala
+++ core/src/main/scala/coulomb/ops/standard/pow.scala
@@ -26,62 +26,45 @@ import coulomb.ops.{Pow, SimplifiedUnit}
 import coulomb.rational.typeexpr
 import coulomb.ops.algebra.FractionalPower
 
-object study1:
-    import coulomb.*, algebra.instances.all.given, coulomb.ops.standard.given, coulomb.policy.standard.given
-    val x = 2d.withUnit[String] 
-    val y = 2f.withUnit[Int] 
-    val z = 2f.withUnit[Boolean] 
+object pow:
+    transparent inline given ctx_pow_FractionalPower[V, U, E](using
+        alg: FractionalPower[V],
+        su: SimplifiedUnit[U ^ E]
+            ): Pow[V, U, E] =
+        new infra.PowFP[V, U, E, su.UO](alg, typeexpr.double[E])
 
-object study2:
-    import coulomb.*, algebra.instances.all.given, coulomb.ops.standard.given, coulomb.policy.standard.given
-    import coulomb.ops.standard.study1.*
-    val a = x.pow[2] 
-    val b = y.pow[3] 
-    val c = z.pow[4] 
+    transparent inline given ctx_pow_MultiplicativeGroup[V, U, E](using
+        nfp: NotGiven[FractionalPower[V]],
+        alg: MultiplicativeGroup[V],
+        aie: typeexpr.AllInt[E],
+        su: SimplifiedUnit[U ^ E]
+            ): Pow[V, U, E] =
+        new infra.PowMG[V, U, E, su.UO](alg, aie.value)
 
-class PowImplFP[V, U, E, UOp](alg: FractionalPower[V], e: Double) extends Pow[V, U, E]:
-    type VO = V
-    type UO = UOp
-    def apply(q: Quantity[V, U]): Quantity[VO, UO] =
-        alg.pow(q.value, e).withUnit[UO]
+    transparent inline given ctx_pow_MultiplicativeSemigroup[V, U, E](using
+        nfp: NotGiven[FractionalPower[V]],
+        nmg: NotGiven[MultiplicativeGroup[V]],
+        alg: MultiplicativeSemigroup[V],
+        pie: typeexpr.PosInt[E],
+        su: SimplifiedUnit[U ^ E]
+            ): Pow[V, U, E] =
+        new infra.PowMSG[V, U, E, su.UO](alg, pie.value)
 
-transparent inline given ctx_pow_FractionalPower[V, U, E](using
-    alg: FractionalPower[V],
-    su: SimplifiedUnit[U ^ E]
-        ): Pow[V, U, E] =
-    new PowImplFP[V, U, E, su.UO](alg, typeexpr.double[E])
-    /*
-    val e = typeexpr.double[E]
-    new Pow[V, U, E]:
-        type VO = V
-        type UO = su.UO
-        def apply(q: Quantity[V, U]): Quantity[VO, UO] =
-            alg.pow(q.value, e).withUnit[UO]
-    */
+    object infra:
+        class PowFP[V, U, E, UOp](alg: FractionalPower[V], e: Double) extends Pow[V, U, E]:
+            type VO = V
+            type UO = UOp
+            def apply(q: Quantity[V, U]): Quantity[VO, UO] =
+                alg.pow(q.value, e).withUnit[UO]
 
-transparent inline given ctx_pow_MultiplicativeGroup[V, U, E](using
-    nfp: NotGiven[FractionalPower[V]],
-    alg: MultiplicativeGroup[V],
-    aie: typeexpr.AllInt[E],
-    su: SimplifiedUnit[U ^ E]
-        ): Pow[V, U, E] =
-    val e = aie.value
-    new Pow[V, U, E]:
-        type VO = V
-        type UO = su.UO
-        def apply(q: Quantity[V, U]): Quantity[VO, UO] =
-            alg.pow(q.value, e).withUnit[UO]
+        class PowMG[V, U, E, UOp](alg: MultiplicativeGroup[V], e: Int) extends Pow[V, U, E]:
+            type VO = V
+            type UO = UOp
+            def apply(q: Quantity[V, U]): Quantity[VO, UO] =
+                alg.pow(q.value, e).withUnit[UO]
 
-transparent inline given ctx_pow_MultiplicativeSemigroup[V, U, E](using
-    nfp: NotGiven[FractionalPower[V]],
-    nmg: NotGiven[MultiplicativeGroup[V]],
-    alg: MultiplicativeSemigroup[V],
-    pie: typeexpr.PosInt[E],
-    su: SimplifiedUnit[U ^ E]
-        ): Pow[V, U, E] =
-    val e = pie.value
-    new Pow[V, U, E]:
-        type VO = V
-        type UO = su.UO
-        def apply(q: Quantity[V, U]): Quantity[VO, UO] =
-            alg.pow(q.value, e).withUnit[UO]
+        class PowMSG[V, U, E, UOp](alg: MultiplicativeSemigroup[V], e: Int) extends Pow[V, U, E]:
+            type VO = V
+            type UO = UOp
+            def apply(q: Quantity[V, U]): Quantity[VO, UO] =
+                alg.pow(q.value, e).withUnit[UO]
