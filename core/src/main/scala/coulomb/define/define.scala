b7524b7adb581cbedab48d78fb2de2394d27ce2b
Erik Erlandson <eerlands@redhat.com>
2022-04-06 07:10:02 -0700

    support named classes with unit declarations

--- core/src/main/scala/coulomb/define/define.scala
+++ core/src/main/scala/coulomb/define/define.scala
@@ -26,21 +26,49 @@ abstract class NamedUnit[Name, Abbv]
  * @tparam U unit type
  * @tparam Name unit name
  * @tparam Abbv unit abbreviation
+ *
+ * {{{
+ * import coulomb.define.*
+ *
+ * // define a unit of spicy heat
+ * type Scoville
+ * given unit_Scoville: BaseUnit[Scoville, "scoville", "sco"] = BaseUnit()
+ * }}}
  */
 abstract class BaseUnit[U, Name, Abbv] extends NamedUnit[Name, Abbv]
+
+/** Companion object utilities for BaseUnit declarations */
+object BaseUnit:
+    def apply[U, Name, Abbv](): BaseUnit[U, Name, Abbv] =
+        new NC[U, Name, Abbv]
+
+    /** internal named class for avoiding anonymous class code generation */
+    class NC[U, Name, Abbv] extends BaseUnit[U, Name, Abbv]
 
 /**
  * @tparam U unit type
  * @tparam D unit it is derived from
  * @tparam Name unit name
  * @tparam Abbv unit abbreviation
+ *
+ * {{{
+ * import coulomb.define.*
+ * import coulomb.units.us.{*, given}
+ *
+ * // a unit of length based on the height of Oliver R. Smoot in 1958
+ * type Smoot
+ * given unit_Smoot: DerivedUnit[Smoot, 67 * Inch, "smoot", "smt"] = DerivedUnit()
+ * }}}
  */
 abstract class DerivedUnit[U, D, Name, Abbv] extends NamedUnit[Name, Abbv]
+
+/** Companion object utilities for DerivedUnit declarations */
 object DerivedUnit:
-    given ctx_unit_to_DU[U, D, Name, Abbv]: scala.Conversion[Unit, DerivedUnit[U, D, Name, Abbv]] =
-      (_: Unit) => new infra.DU[U, D, Name, Abbv]
-    object infra:
-        class DU[U, D, Name, Abbv] extends DerivedUnit[U, D, Name, Abbv]
+    def apply[U, D, Name, Abbv](): DerivedUnit[U, D, Name, Abbv] =
+        new NC[U, D, Name, Abbv]
+
+    /** internal named class for avoiding anonymous class code generation */
+    class NC[U, D, Name, Abbv] extends DerivedUnit[U, D, Name, Abbv]
 
 /**
  * Delta Units represent units with an offset in their transforms, for example temperatures or times
@@ -50,6 +78,20 @@ object DerivedUnit:
  * @tparam O unit transform offset
  * @tparam Name unit name
  * @tparam Abbv unit abbreviation
+ * {{{
+ * import coulomb.define.*
+ * import coulomb.units.si.{*, given}
+ *
+ * type Fahrenheit
+ * given unit_Fahrenheit: DeltaUnit[Fahrenheit, (5 / 9) * Kelvin, 45967 / 100, "fahrenheit", "F"] = DeltaUnit()
+ * }}}
  */
 abstract class DeltaUnit[U, D, O, Name, Abbv] extends DerivedUnit[U, D, Name, Abbv]
 
+/** Companion object utilities for DeltaUnit declarations */
+object DeltaUnit:
+    def apply[U, D, O, Name, Abbv](): DeltaUnit[U, D, O, Name, Abbv] =
+        new NC[U, D, O, Name, Abbv]
+
+    /** internal named class for avoiding anonymous class code generation */
+    class NC[U, D, O, Name, Abbv] extends DeltaUnit[U, D, O, Name, Abbv]
