b1330e5e5c2c2bdc98ce6e3a83c30bc604a3a67e
Erik Erlandson <eerlands@redhat.com>
2022-04-11 10:43:59 -0700

    Algebraic and Real unit conversion

--- spire/src/main/scala/coulomb/conversion/spire/unit.scala
+++ spire/src/main/scala/coulomb/conversion/spire/unit.scala
@@ -19,7 +19,7 @@ package coulomb.conversion.spire
 object unit:
     import coulomb.conversion.*
     import coulomb.conversion.spire.coefficients.*
-    import _root_.spire.math.Rational
+    import _root_.spire.math.*
 
     export coulomb.conversion.standard.unit.given
 
@@ -29,9 +29,21 @@ object unit:
     inline given ctx_UC_BigDecimal[UF, UT]: UnitConversion[BigDecimal, UF, UT] =
         new infra.BigDecimalUC[UF, UT](coefficientBigDecimal[UF, UT])
 
+    inline given ctx_UC_Algebraic[UF, UT]: UnitConversion[Algebraic, UF, UT] =
+        new infra.AlgebraicUC[UF, UT](Algebraic(coefficientRational[UF, UT]))
+
+    inline given ctx_UC_Real[UF, UT]: UnitConversion[Real, UF, UT] =
+        new infra.RealUC[UF, UT](Real(coefficientRational[UF, UT]))
+
     object infra:
         class RationalUC[UF, UT](c: Rational) extends UnitConversion[Rational, UF, UT]:
             def apply(v: Rational): Rational = c * v
 
         class BigDecimalUC[UF, UT](c: BigDecimal) extends UnitConversion[BigDecimal, UF, UT]:
             def apply(v: BigDecimal): BigDecimal = c * v
+
+        class AlgebraicUC[UF, UT](c: Algebraic) extends UnitConversion[Algebraic, UF, UT]:
+            def apply(v: Algebraic): Algebraic = c * v
+
+        class RealUC[UF, UT](c: Real) extends UnitConversion[Real, UF, UT]:
+            def apply(v: Real): Real = c * v
