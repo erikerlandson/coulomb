ed84a4dd10f3a365a1f1ea8eb6ccc45ce26a1903
Erik Erlandson <eerlands@redhat.com>
2022-04-18 09:53:43 -0700

    ValuePromotionPolicy

--- core/src/main/scala/coulomb/ops/ops.scala
+++ core/src/main/scala/coulomb/ops/ops.scala
@@ -94,6 +94,87 @@ abstract class DeltaOrd[B, VL, UL, VR, UR] extends ((DeltaQuantity[VL, UL, B], D
 abstract class ValueResolution[VL, VR]:
     type VO
 
+object ValueResolution:
+    transparent inline given ctx_VR_XpX[V]: ValueResolution[V, V] = new NC[V, V, V]
+    transparent inline given ctx_VR_LpR[VL, VR](using ValuePromotion[VL, VR]): ValueResolution[VL, VR] = new NC[VL, VR, VR]
+    transparent inline given ctx_VR_RpL[VL, VR](using ValuePromotion[VR, VL]): ValueResolution[VL, VR] = new NC[VL, VR, VL]
+    class NC[VL, VR, VOp] extends ValueResolution[VL, VR]:
+        type VO = VOp
+
 @implicitNotFound("Unable to simplify unit type ${U}")
 abstract class SimplifiedUnit[U]:
     type UO
+
+class ValuePromotion[VF, VT]
+
+object ValuePromotion:
+    import scala.quoted.*
+    import scala.language.implicitConversions
+
+    import coulomb.infra.meta.typestr
+
+    final type &:[H, T]
+    final type TNil
+ 
+    type VppSet[T] = scala.collection.mutable.HashSet[T]
+    val VppSet = scala.collection.mutable.HashSet
+
+    transparent inline given ctx_VP_Path[VF, VT]: ValuePromotion[VF, VT] = ${ vpPath[VF, VT] }
+
+    def vpPath[VF, VT](using Quotes, Type[VF], Type[VT]): Expr[ValuePromotion[VF, VT]] =
+        import quotes.reflect.*
+        val (vf, vt) = (TypeRepr.of[VF], TypeRepr.of[VT])
+        if (pathexists(vf.typeSymbol.fullName,
+                       vt.typeSymbol.fullName,
+                       getvpp))
+            '{ new ValuePromotion[VF, VT] }
+        else
+            report.error(s"no promotion from ${typestr(vf)} => ${typestr(vt)}")
+            '{ new ValuePromotion[VF, VT] }
+
+    def getvpp(using Quotes): VppSet[(String, String)] =
+        import quotes.reflect.*
+        Implicits.search(TypeRepr.of[ValuePromotionPolicy].appliedTo(List(TypeBounds.empty))) match
+            case iss: ImplicitSearchSuccess =>
+                val AppliedType(_, List(vppt)) = iss.tree.tpe.baseType(TypeRepr.of[ValuePromotionPolicy].typeSymbol)
+                vpp2str(vppt)
+            case _ =>
+                report.error("no ValuePromotionPolicy was found in scope")
+                VppSet.empty[(String, String)]
+
+    def vpp2str(using Quotes)(vpp: quotes.reflect.TypeRepr): VppSet[(String, String)] =
+        import quotes.reflect.*
+        vpp match
+            case t if (t =:= TypeRepr.of[TNil]) => VppSet.empty[(String, String)]
+            case AppliedType(v, List(AppliedType(t2, List(vf, vt)), tail)) if ((v =:= TypeRepr.of[&:]) && (t2 =:= TypeRepr.of[Tuple2])) =>
+                val vppset = vpp2str(tail)
+                vppset.add((vf.typeSymbol.fullName, vt.typeSymbol.fullName))
+                vppset
+            case _ =>
+                report.error(s"type ${typestr(vpp)} is not a valid value promotion policy")
+                VppSet.empty[(String, String)]
+
+    def pathexists(vf: String, vt: String, edges: VppSet[(String, String)]): Boolean =
+        val reachable = VppSet(vf)
+        var haspath = false
+        var done = false
+        while (!done) do
+            val prevsize = reachable.size
+            val next = reachable.flatMap(r => edges.filter { (f, _) => f == r }.map { (_, t) => t })
+            reachable.addAll(next)
+            if (reachable.contains(vt))
+                haspath = true
+                done = true
+            if (reachable.size == prevsize)
+                done = true
+        haspath
+
+class ValuePromotionPolicy[Pairs]
+object ValuePromotionPolicy:
+    def apply[P](): ValuePromotionPolicy[P] = new ValuePromotionPolicy[P] {}
+
+object test:
+    import ValuePromotion.{ &:, TNil }
+    given ValuePromotionPolicy[
+        (Int, Long) &: (Long, Float) &: (Float, Double) &: TNil
+    ] = ValuePromotionPolicy()
--- core/src/main/scala/coulomb/ops/resolution/standard.scala
+++ core/src/main/scala/coulomb/ops/resolution/standard.scala
@@ -17,56 +17,10 @@
 package coulomb.ops.resolution
 
 object standard:
-    import coulomb.ops.ValueResolution
+    import coulomb.ops.ValuePromotionPolicy
+    import coulomb.ops.ValuePromotion.{ &:, TNil }
 
-    transparent inline given ctx_VR_Double_Double: ValueResolution[Double, Double] =
-        new infra.VRNC[Double, Double, Double]
-
-    transparent inline given ctx_VR_Double_Float: ValueResolution[Double, Float] =
-        new infra.VRNC[Double, Float, Double]
-
-    transparent inline given ctx_VR_Double_Long: ValueResolution[Double, Long] =
-        new infra.VRNC[Double, Long, Double]
-
-    transparent inline given ctx_VR_Double_Int: ValueResolution[Double, Int] =
-        new infra.VRNC[Double, Int, Double]
-
-    transparent inline given ctx_VR_Float_Double: ValueResolution[Float, Double] =
-        new infra.VRNC[Float, Double, Double]
-
-    transparent inline given ctx_VR_Float_Float: ValueResolution[Float, Float] =
-        new infra.VRNC[Float, Float, Float]
-
-    transparent inline given ctx_VR_Float_Long: ValueResolution[Float, Long] =
-        new infra.VRNC[Float, Long, Float]
-
-    transparent inline given ctx_VR_Float_Int: ValueResolution[Float, Int] =
-        new infra.VRNC[Float, Int, Float]
-
-    transparent inline given ctx_VR_Long_Double: ValueResolution[Long, Double] =
-        new infra.VRNC[Long, Double, Double]
-
-    transparent inline given ctx_VR_Long_Float: ValueResolution[Long, Float] =
-        new infra.VRNC[Long, Float, Float]
-
-    transparent inline given ctx_VR_Long_Long: ValueResolution[Long, Long] =
-        new infra.VRNC[Long, Long, Long]
-
-    transparent inline given ctx_VR_Long_Int: ValueResolution[Long, Int] =
-        new infra.VRNC[Long, Int, Long]
-
-    transparent inline given ctx_VR_Int_Double: ValueResolution[Int, Double] =
-        new infra.VRNC[Int, Double, Double]
-
-    transparent inline given ctx_VR_Int_Float: ValueResolution[Int, Float] =
-        new infra.VRNC[Int, Float, Float]
-
-    transparent inline given ctx_VR_Int_Long: ValueResolution[Int, Long] =
-        new infra.VRNC[Int, Long, Long]
-
-    transparent inline given ctx_VR_Int_Int: ValueResolution[Int, Int] =
-        new infra.VRNC[Int, Int, Int]
-
-    object infra:
-        class VRNC[VL, VR, VOp] extends ValueResolution[VL, VR]:
-            type VO = VOp
+    // ValuePromotion infers the transitive closure of all promotions
+    given ctx_vpp_standard: ValuePromotionPolicy[
+        (Int, Long) &: (Long, Float) &: (Float, Double) &: TNil
+    ] = ValuePromotionPolicy()
