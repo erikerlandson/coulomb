d1d74ca02f9f5f00339971376b1428c9aa75d423
Erik Erlandson <eerlands@redhat.com>
2022-04-30 11:33:55 -0700

    test spire addition

--- spire/src/test/scala/coulomb/quantity.scala
+++ spire/src/test/scala/coulomb/quantity.scala
@@ -65,6 +65,42 @@ class SpireQuantitySuite extends CoulombSuite:
 
     test("addition strict") {
         import coulomb.policy.spire.strict.given
+
         (Rational(1).withUnit[Meter] + Rational(1).withUnit[Meter]).assertQ[Rational, Meter](Rational(2))
+        (BigInt(1).withUnit[Meter] + BigInt(1).withUnit[Meter]).assertQ[BigInt, Meter](BigInt(2))
+        (BigDecimal(1).withUnit[Meter] + BigDecimal(1).withUnit[Meter]).assertQ[BigDecimal, Meter](BigDecimal(2))
+        (Algebraic(1).withUnit[Meter] + Algebraic(1).withUnit[Meter]).assertQ[Algebraic, Meter](Algebraic(2))
+        (Real(1).withUnit[Meter] + Real(1).withUnit[Meter]).assertQ[Real, Meter](Real(2))
+
+        assertCE("Real(1).withUnit[Meter] + Rational(1).withUnit[Meter]")
+        assertCE("Rational(1).withUnit[Meter] + Rational(1).withUnit[Yard]")
+
+        (Real(1).withUnit[Meter].toValue[Rational] + Rational(1).withUnit[Meter]).assertQ[Rational, Meter](Rational(2))
+        (Rational(1).withUnit[Meter] + Rational(1).withUnit[Yard].toUnit[Meter]).assertQ[Rational, Meter](Rational(19144, 10000))
+    }
+
+    test("addition standard") {
+        import coulomb.policy.spire.standard.given
+
+        (Rational(1).withUnit[Meter] + Rational(1).withUnit[Meter]).assertQ[Rational, Meter](Rational(2))
+
+        (Rational(1).withUnit[Meter] + Rational(1).withUnit[Yard]).assertQ[Rational, Meter](Rational(19144, 10000))
+        (BigDecimal(1).withUnit[Meter] + BigDecimal(1).withUnit[Yard]).assertQ[BigDecimal, Meter](BigDecimal(1.9144))
+
+        (1.withUnit[Meter] + BigInt(1).withUnit[Meter]).assertQ[BigInt, Meter](BigInt(2))
+        (BigInt(1).withUnit[Meter] + 1.withUnit[Meter]).assertQ[BigInt, Meter](BigInt(2))
+        (1f.withUnit[Meter] + BigDecimal(1).withUnit[Meter]).assertQ[BigDecimal, Meter](BigDecimal(2))
+        (BigDecimal(1).withUnit[Meter] + 1f.withUnit[Meter]).assertQ[BigDecimal, Meter](BigDecimal(2))
+        (1.withUnit[Meter] + Real(1).withUnit[Meter]).assertQ[Real, Meter](Real(2))
+        (Real(1).withUnit[Meter] + 1.withUnit[Meter]).assertQ[Real, Meter](Real(2))
+
+        (Rational(1).withUnit[Meter] + BigDecimal(1).withUnit[Yard]).assertQ[Rational, Meter](Rational(19144, 10000))
+        (BigDecimal(1).withUnit[Meter] + Rational(1).withUnit[Yard]).assertQ[Rational, Meter](Rational(19144, 10000))
+
+        assertCE("BigDecimal(1).withUnit[Meter] + BigDecimal.withUnit[Second]")
+        
+        assertCE("BigInt(1).withUnit[Meter] + BigInt(1).withUnit[Yard]")
+
+        (BigInt(1).withUnit[Meter] + BigInt(1).withUnit[Yard].tToUnit[Meter]).assertQ[BigInt, Meter](BigInt(1))
     }
 
