f066a20b16d1315414594dd4d1f4b391ac67f982
Erik Erlandson <eerlands@redhat.com>
2022-07-09 12:38:28 -0700

    support lifted unitless values

--- core/src/main/scala/coulomb/ops/algebra/int.scala
+++ core/src/main/scala/coulomb/ops/algebra/int.scala
@@ -16,7 +16,11 @@
 
 package coulomb.ops.algebra
 
-import algebra.ring.TruncatedDivision
+import _root_.algebra.ring.TruncatedDivision
+
+import coulomb.*
+import coulomb.syntax.*
+import coulomb.ops.*
 
 object int:
     given ctx_Int_is_TruncatingPower: TruncatingPower[Int] with
@@ -29,7 +33,14 @@ object int:
         def fquot(x: Int, y: Int): Int = ???
         def fmod(x: Int, y: Int): Int = ???
         def abs(a: Int): Int = ???
-        def additiveCommutativeMonoid: algebra.ring.AdditiveCommutativeMonoid[Int] = ???
+        def additiveCommutativeMonoid: _root_.algebra.ring.AdditiveCommutativeMonoid[Int] = ???
         def order: _root_.cats.kernel.Order[Int] = ???
         def signum(a: Int): Int = ???
+
+    extension(vl: Int)
+        transparent inline def *[VR, UR](qr: Quantity[VR, UR])(using mul: Mul[Int, 1, VR, UR]): Quantity[mul.VO, mul.UO] =
+            mul.eval(vl.withUnit[1], qr)
+
+        transparent inline def /[VR, UR](qr: Quantity[VR, UR])(using div: Div[Int, 1, VR, UR]): Quantity[div.VO, div.UO] =
+            div.eval(vl.withUnit[1], qr)
 
