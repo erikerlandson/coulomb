c5c248e39c4b7caff479c17942b34bde6373e9fd
Erik Erlandson <eerlands@redhat.com>
2022-11-20 16:04:17 -0700

    refined algebraic policy

---
+++ refined/src/main/scala/coulomb/policy/overlay/refined/policy.scala
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2022 Erik Erlandson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package coulomb.policy.overlay.refined
+
+object algebraic:
+    export coulomb.ops.algebra.refined.all.given
+    export coulomb.ops.resolution.refined.given
+    export coulomb.conversion.refined.value.given
+    export coulomb.conversion.refined.unit.given
