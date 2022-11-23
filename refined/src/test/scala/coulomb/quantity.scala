58b12e4961fcb73fc7f0dc98096b11abd2cd6578
Erik Erlandson <eerlands@redhat.com>
2022-11-23 14:00:57 -0700

    toValue, toUnit, value, show

--- refined/src/test/scala/coulomb/quantity.scala
+++ refined/src/test/scala/coulomb/quantity.scala
@@ -46,27 +46,65 @@ class RefinedQuantityAlgebraicSuite extends CoulombSuite:
         1.withRP[NonNegative].withUnit[Meter].assertQ[Refined[Int, NonNegative], Meter](1.withRP[NonNegative])
     }
 
+    test("value") {
+        1.withRP[Positive].withUnit[Meter].value.assertVT[Refined[Int, Positive]](1.withRP[Positive])
+    }
+
+    test("show") {
+        assertEquals(1.withRP[Positive].withUnit[Meter].show, "1 m")
+    }
+
+    test("showFull") {
+        assertEquals(1.withRP[NonNegative].withUnit[Second].showFull, "1 second")
+    }
+
+    test("toValue") {
+        import coulomb.policy.strict.given
+        import coulomb.policy.overlay.refined.algebraic.given
+
+        1.withRP[Positive].withUnit[Meter].toValue[Refined[Double, Positive]]
+            .assertQ[Refined[Double, Positive], Meter](1d.withRP[Positive])
+        1L.withRP[NonNegative].withUnit[Meter].toValue[Refined[Float, NonNegative]]
+            .assertQ[Refined[Float, NonNegative], Meter](1f.withRP[NonNegative])
+
+        assertCE("1d.withRP[Positive].withUnit[Meter].toValue[Refined[Int, Positive]]")
+
+        1.5.withRP[Positive].withUnit[Meter].tToValue[Refined[Int, Positive]]
+            .assertQ[Refined[Int, Positive], Meter](1.withRP[Positive])
+        1.5f.withRP[NonNegative].withUnit[Meter].tToValue[Refined[Int, NonNegative]]
+            .assertQ[Refined[Int, NonNegative], Meter](1.withRP[NonNegative])
+    }
+
+    test("toUnit") {
+        import coulomb.policy.strict.given
+        import coulomb.policy.overlay.refined.algebraic.given
+
+        1d.withRP[Positive].withUnit[Kilo * Meter].toUnit[Meter]
+            .assertQ[Refined[Double, Positive], Meter](1000d.withRP[Positive])
+        1f.withRP[NonNegative].withUnit[Kilo * Meter].toUnit[Meter]
+            .assertQ[Refined[Float, NonNegative], Meter](1000f.withRP[NonNegative])
+
+        assertCE("1.withRP[Positive].withUnit[Kilo * Meter].toUnit[Meter]")
+
+        1.withRP[Positive].withUnit[Meter].tToUnit[Yard]
+            .assertQ[Refined[Int, Positive], Yard](1.withRP[Positive])
+        1.withRP[NonNegative].withUnit[Meter].tToUnit[Yard]
+            .assertQ[Refined[Int, NonNegative], Yard](1.withRP[NonNegative])
+    }
+
     test("add strict") {
         import coulomb.policy.strict.given
         import coulomb.policy.overlay.refined.algebraic.given
 
         (1d.withRP[Positive].withUnit[Meter] + 2d.withRP[Positive].withUnit[Meter])
             .assertQ[Refined[Double, Positive], Meter](3d.withRP[Positive])
-        (1f.withRP[Positive].withUnit[Meter] + 2f.withRP[Positive].withUnit[Meter])
-            .assertQ[Refined[Float, Positive], Meter](3f.withRP[Positive])
-        (1L.withRP[Positive].withUnit[Meter] + 2L.withRP[Positive].withUnit[Meter])
-            .assertQ[Refined[Long, Positive], Meter](3L.withRP[Positive])
         (1.withRP[Positive].withUnit[Meter] + 2.withRP[Positive].withUnit[Meter])
             .assertQ[Refined[Int, Positive], Meter](3.withRP[Positive])
 
-        (1d.withRP[NonNegative].withUnit[Meter] + 2d.withRP[NonNegative].withUnit[Meter])
-            .assertQ[Refined[Double, NonNegative], Meter](3d.withRP[NonNegative])
         (1f.withRP[NonNegative].withUnit[Meter] + 2f.withRP[NonNegative].withUnit[Meter])
             .assertQ[Refined[Float, NonNegative], Meter](3f.withRP[NonNegative])
         (1L.withRP[NonNegative].withUnit[Meter] + 2L.withRP[NonNegative].withUnit[Meter])
             .assertQ[Refined[Long, NonNegative], Meter](3L.withRP[NonNegative])
-        (1.withRP[NonNegative].withUnit[Meter] + 2.withRP[NonNegative].withUnit[Meter])
-            .assertQ[Refined[Int, NonNegative], Meter](3.withRP[NonNegative])
 
         // differing value or unit types are not supported in strict policy
         assertCE("1.withRP[Positive].withUnit[Meter] + 2d.withRP[Positive].withUnit[Meter]")
