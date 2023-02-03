6ea4503403822f36932e8385787ea52e9a91d14b
Erik Erlandson <eerlands@redhat.com>
2023-02-03 15:47:14 -0700

    given staging.Compiler from above

--- runtime/src/main/scala/coulomb/runtime/runtime.scala
+++ runtime/src/main/scala/coulomb/runtime/runtime.scala
@@ -51,6 +51,7 @@ object RuntimeQuantity:
 
 extension [VL](ql: RuntimeQuantity[VL])
     inline def toQuantity[VR, UR](using
+        scmp: staging.Compiler,
         vi: ValueConversion[VL, Rational],
         vo: ValueConversion[Rational, VR]
     ): Either[String, Quantity[VR, UR]] =
@@ -65,182 +66,177 @@ package syntax {
 }
 
 package conversion {
-abstract class RuntimeUnitConversion[V] extends (V => V)
-object RuntimeUnitConversion:
-    def apply[V](rtuF: RuntimeUnit, rtuT: RuntimeUnit)(using
-        scmp: staging.Compiler,
-        vi: ValueConversion[V, Rational],
-        vo: ValueConversion[Rational, V]
-    ): Either[String, RuntimeUnitConversion[V]] =
-        coefficients.coefficientRational(rtuF, rtuT).map { coef =>
-            new RuntimeUnitConversion[V] {
-                def apply(v: V): V = vo(coef * vi(v))
+    abstract class RuntimeUnitConversion[V] extends (V => V)
+    object RuntimeUnitConversion:
+        def apply[V](uf: RuntimeUnit, ut: RuntimeUnit)(using
+            scmp: staging.Compiler,
+            vi: ValueConversion[V, Rational],
+            vo: ValueConversion[Rational, V]
+        ): Either[String, RuntimeUnitConversion[V]] =
+            coefficients.coefficientRational(uf, ut).map { coef =>
+                new RuntimeUnitConversion[V] {
+                    def apply(v: V): V = vo(coef * vi(v))
+                }
             }
-        }
 
-object coefficients:
-    import coulomb.runtime.infra.meta.*
-    import coulomb.conversion.coefficients.{coefficientRational => staticCR}
+    object coefficients:
+        import coulomb.runtime.infra.meta.*
+        import coulomb.conversion.coefficients.{coefficientRational => staticCR}
 
-    def coefficientRational(uf: RuntimeUnit, ut: RuntimeUnit)(using
-        staging.Compiler
-    ): Either[String, Rational] =
-        Try {
-            staging.run {
-                import quotes.reflect.*
-                (rtuTypeRepr(uf).asType, rtuTypeRepr(ut).asType) match
-                    case ('[f], '[t]) =>
-                        '{ staticCR[f, t] }
-            }
-        } match
-            case Success(coef) => Right(coef)
-            case Failure(e) => Left(e.getMessage)
+        def coefficientRational(uf: RuntimeUnit, ut: RuntimeUnit)(using
+            staging.Compiler
+        ): Either[String, Rational] =
+            Try {
+                staging.run {
+                    import quotes.reflect.*
+                    (rtuTypeRepr(uf).asType, rtuTypeRepr(ut).asType) match
+                        case ('[f], '[t]) => '{ staticCR[f, t] }
+                }
+            } match
+                case Success(coef) => Right(coef)
+                case Failure(e)    => Left(e.getMessage)
 }
 
 package infra {
 
-object meta:
-    import scala.unchecked
-    import scala.language.implicitConversions
+    object meta:
+        import scala.unchecked
+        import scala.language.implicitConversions
 
-    import coulomb.infra.meta.{*, given}
+        import coulomb.infra.meta.{*, given}
 
-    given ctx_RuntimeUnitToExpr: ToExpr[RuntimeUnit] with
-        def apply(rtu: RuntimeUnit)(using Quotes): Expr[RuntimeUnit] =
+        given ctx_RuntimeUnitToExpr: ToExpr[RuntimeUnit] with
+            def apply(rtu: RuntimeUnit)(using Quotes): Expr[RuntimeUnit] =
+                rtu match
+                    case RuntimeUnit.UnitType(path) =>
+                        '{ RuntimeUnit.UnitType(${ Expr(path) }) }
+                    case RuntimeUnit.Mul(l, r) =>
+                        '{ RuntimeUnit.Mul(${ Expr(l) }, ${ Expr(r) }) }
+                    case RuntimeUnit.Div(n, d) =>
+                        '{ RuntimeUnit.Div(${ Expr(n) }, ${ Expr(d) }) }
+                    case RuntimeUnit.Pow(b, e) =>
+                        '{ RuntimeUnit.Pow(${ Expr(b) }, ${ Expr(e) }) }
+
+        inline def coefExpr[UT](uf: RuntimeUnit)(using
+            scmp: staging.Compiler
+        ): Either[String, Rational] =
+            ${ coefExprMeta[UT]('uf, 'scmp) }
+
+        def coefExprMeta[UT](
+            uf: Expr[RuntimeUnit],
+            scmp: Expr[staging.Compiler]
+        )(using
+            Quotes,
+            Type[UT]
+        ): Expr[Either[String, Rational]] =
+            import quotes.reflect.*
+            import coulomb.runtime.conversion.coefficients.coefficientRational
+            val ut = typeReprRTU(TypeRepr.of[UT])
+            '{ coefficientRational($uf, ${ Expr(ut) })(using $scmp) }
+
+        def unitRTU[U](using Quotes, Type[U]): Expr[RuntimeUnit] =
+            import quotes.reflect.*
+            Expr(typeReprRTU(TypeRepr.of[U]))
+
+        def rtuTypeRepr(using Quotes)(
+            rtu: RuntimeUnit
+        ): quotes.reflect.TypeRepr =
+            import quotes.reflect.*
             rtu match
-                case RuntimeUnit.UnitType(path) =>
-                    '{ RuntimeUnit.UnitType(${ Expr(path) }) }
+                case RuntimeUnit.UnitType(path) => fqTypeRepr(path)
                 case RuntimeUnit.Mul(l, r) =>
-                    '{ RuntimeUnit.Mul(${ Expr(l) }, ${ Expr(r) }) }
+                    val ltr = rtuTypeRepr(l)
+                    val rtr = rtuTypeRepr(r)
+                    TypeRepr.of[coulomb.`*`].appliedTo(List(ltr, rtr))
                 case RuntimeUnit.Div(n, d) =>
-                    '{ RuntimeUnit.Div(${ Expr(n) }, ${ Expr(d) }) }
+                    val ntr = rtuTypeRepr(n)
+                    val dtr = rtuTypeRepr(d)
+                    TypeRepr.of[coulomb.`/`].appliedTo(List(ntr, dtr))
                 case RuntimeUnit.Pow(b, e) =>
-                    '{ RuntimeUnit.Pow(${ Expr(b) }, ${ Expr(e) }) }
+                    val btr = rtuTypeRepr(b)
+                    val etr = rationalTE(e)
+                    TypeRepr.of[coulomb.`^`].appliedTo(List(btr, etr))
 
-    inline def coefExpr[UT](uf: RuntimeUnit): Either[String, Rational] =
-        ${ coefExprMeta[UT]('uf) }
+        def typeReprRTU(using Quotes)(
+            tr: quotes.reflect.TypeRepr
+        ): RuntimeUnit =
+            import quotes.reflect.*
+            tr match
+                case AppliedType(op, List(lu, ru))
+                    if (op =:= TypeRepr.of[coulomb.`*`]) =>
+                    RuntimeUnit.Mul(typeReprRTU(lu), typeReprRTU(ru))
+                case AppliedType(op, List(lu, ru))
+                    if (op =:= TypeRepr.of[coulomb.`/`]) =>
+                    RuntimeUnit.Div(typeReprRTU(lu), typeReprRTU(ru))
+                case AppliedType(op, List(b, e))
+                    if (op =:= TypeRepr.of[coulomb.`^`]) =>
+                    val rationalTE(ev) = e: @unchecked
+                    RuntimeUnit.Pow(typeReprRTU(b), ev)
+                case t =>
+                    // should add checking for types with type-args here
+                    // possibly an explicit non dealirtuing policy here would allow
+                    // parameterized types to be handled via typedef aliases?
+                    RuntimeUnit.UnitType(t.typeSymbol.fullName)
 
-    def coefExprMeta[UT](uf: Expr[RuntimeUnit])(using
-        Quotes,
-        Type[UT]
-    ): Expr[Either[String, Rational]] =
-        import quotes.reflect.*
-        import coulomb.runtime.conversion.coefficients.coefficientRational
-        val cmp = stagingCompiler
-        val ut = typeReprRTU(TypeRepr.of[UT])
-        '{ coefficientRational($uf, ${ Expr(ut) })(using $cmp) }
+        def fqTypeRepr(using Quotes)(path: String): quotes.reflect.TypeRepr =
+            fqTypeRepr(path.split('.').toIndexedSeq)
 
-    def stagingCompiler(using Quotes): Expr[staging.Compiler] =
-        import quotes.reflect.*
-        Implicits.search(TypeRepr.of[staging.Compiler]) match
-            case iss: ImplicitSearchSuccess =>
-                iss.tree.asExprOf[staging.Compiler]
-            case _ =>
-                report.errorAndAbort("no 'given' staging.Compiler is in scope")
-                // I'm not even sorry.
-                null.asInstanceOf[Expr[staging.Compiler]]
+        def fqFieldSymbol(using Quotes)(path: String): quotes.reflect.Symbol =
+            fqFieldSymbol(path.split('.').toIndexedSeq)
 
-    def unitRTU[U](using Quotes, Type[U]): Expr[RuntimeUnit] =
-        import quotes.reflect.*
-        Expr(typeReprRTU(TypeRepr.of[U]))
-
-    def rtuTypeRepr(using Quotes)(
-        rtu: RuntimeUnit
-    ): quotes.reflect.TypeRepr =
-        import quotes.reflect.*
-        rtu match
-            case RuntimeUnit.UnitType(path) => fqTypeRepr(path)
-            case RuntimeUnit.Mul(l, r) =>
-                val ltr = rtuTypeRepr(l)
-                val rtr = rtuTypeRepr(r)
-                TypeRepr.of[coulomb.`*`].appliedTo(List(ltr, rtr))
-            case RuntimeUnit.Div(n, d) =>
-                val ntr = rtuTypeRepr(n)
-                val dtr = rtuTypeRepr(d)
-                TypeRepr.of[coulomb.`/`].appliedTo(List(ntr, dtr))
-            case RuntimeUnit.Pow(b, e) =>
-                val btr = rtuTypeRepr(b)
-                val etr = rationalTE(e)
-                TypeRepr.of[coulomb.`^`].appliedTo(List(btr, etr))
-
-    def typeReprRTU(using Quotes)(
-        tr: quotes.reflect.TypeRepr
-    ): RuntimeUnit =
-        import quotes.reflect.*
-        tr match
-            case AppliedType(op, List(lu, ru))
-                if (op =:= TypeRepr.of[coulomb.`*`]) =>
-                RuntimeUnit.Mul(typeReprRTU(lu), typeReprRTU(ru))
-            case AppliedType(op, List(lu, ru))
-                if (op =:= TypeRepr.of[coulomb.`/`]) =>
-                RuntimeUnit.Div(typeReprRTU(lu), typeReprRTU(ru))
-            case AppliedType(op, List(b, e))
-                if (op =:= TypeRepr.of[coulomb.`^`]) =>
-                val rationalTE(ev) = e: @unchecked
-                RuntimeUnit.Pow(typeReprRTU(b), ev)
-            case t =>
-                // should add checking for types with type-args here
-                // possibly an explicit non dealirtuing policy here would allow
-                // parameterized types to be handled via typedef aliases?
-                RuntimeUnit.UnitType(t.typeSymbol.fullName)
-
-    def fqTypeRepr(using Quotes)(path: String): quotes.reflect.TypeRepr =
-        fqTypeRepr(path.split('.').toIndexedSeq)
-
-    def fqFieldSymbol(using Quotes)(path: String): quotes.reflect.Symbol =
-        fqFieldSymbol(path.split('.').toIndexedSeq)
-
-    def fqTypeRepr(using Quotes)(
-        path: Seq[String]
-    ): quotes.reflect.TypeRepr =
-        import quotes.reflect.*
-        if (path.isEmpty)
-            report.errorAndAbort("fqTypeRepr: empty path")
-            TypeRepr.of[Unit]
-        else
-            val q = fqFieldSymbol(path.dropRight(1))
-            val qt = q.typeMembers.filter(_.name == path.last)
-            if (qt.length == 1) qt.head.typeRef
-            else
-                report.errorAndAbort(
-                    s"""fqTypeRepr: bad path ${path.mkString(
-                            "."
-                        )} at ${path.last}"""
-                )
+        def fqTypeRepr(using Quotes)(
+            path: Seq[String]
+        ): quotes.reflect.TypeRepr =
+            import quotes.reflect.*
+            if (path.isEmpty)
+                report.errorAndAbort("fqTypeRepr: empty path")
                 TypeRepr.of[Unit]
-
-    def fqFieldSymbol(using Quotes)(
-        path: Seq[String]
-    ): quotes.reflect.Symbol =
-        import quotes.reflect.*
-        def work(q: Symbol, tail: Seq[String]): Symbol =
-            if (tail.isEmpty) q
             else
-                // look for modules first
-                // this includes packages and objects
-                val qt = q.declarations.filter { x =>
-                    (x.name == tail.head) && x.flags.is(Flags.Module)
-                }
-                // there are sometimes two symbols representing a module
-                // is the difference important to this function?
-                if (qt.length > 0) work(qt.head, tail.tail)
+                val q = fqFieldSymbol(path.dropRight(1))
+                val qt = q.typeMembers.filter(_.name == path.last)
+                if (qt.length == 1) qt.head.typeRef
                 else
-                    // if no module exists, look for declared symbol
-                    val f = q.declarations.filter { x => x.name == tail.head }
-                    // expect a unique field declaration in this case
-                    if ((f.length == 1) && (tail.length == 1)) f.head
+                    report.errorAndAbort(
+                        s"""fqTypeRepr: bad path ${path.mkString(
+                                "."
+                            )} at ${path.last}"""
+                    )
+                    TypeRepr.of[Unit]
+
+        def fqFieldSymbol(using Quotes)(
+            path: Seq[String]
+        ): quotes.reflect.Symbol =
+            import quotes.reflect.*
+            def work(q: Symbol, tail: Seq[String]): Symbol =
+                if (tail.isEmpty) q
+                else
+                    // look for modules first
+                    // this includes packages and objects
+                    val qt = q.declarations.filter { x =>
+                        (x.name == tail.head) && x.flags.is(Flags.Module)
+                    }
+                    // there are sometimes two symbols representing a module
+                    // is the difference important to this function?
+                    if (qt.length > 0) work(qt.head, tail.tail)
                     else
-                        // if we cannot find a field here, it is a failure
-                        report.errorAndAbort(
-                            s"""fqFieldSymbol: bad path ${path.mkString(
-                                    "."
-                                )} at ${tail.head}"""
-                        )
-                        defn.RootPackage
-        if (path.isEmpty)
-            defn.RootPackage
-        else if (path.head == "_root_")
-            work(defn.RootPackage, path.tail)
-        else
-            work(defn.RootPackage, path)
+                        // if no module exists, look for declared symbol
+                        val f = q.declarations.filter { x =>
+                            x.name == tail.head
+                        }
+                        // expect a unique field declaration in this case
+                        if ((f.length == 1) && (tail.length == 1)) f.head
+                        else
+                            // if we cannot find a field here, it is a failure
+                            report.errorAndAbort(
+                                s"""fqFieldSymbol: bad path ${path.mkString(
+                                        "."
+                                    )} at ${tail.head}"""
+                            )
+                            defn.RootPackage
+            if (path.isEmpty)
+                defn.RootPackage
+            else if (path.head == "_root_")
+                work(defn.RootPackage, path.tail)
+            else
+                work(defn.RootPackage, path)
 }
